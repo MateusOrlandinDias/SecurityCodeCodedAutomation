188f902a0b1952233b5ecea1ecc31357415286e3
MateusOrlandinDias <mateus.orlandin@gmail.com>
2024-03-23 17:12:56 -0300

    Remove Base folder

--- SecurityCode/.local/.codedworkflows/WorkflowRunnerService.cs
+++ SecurityCode/.local/.codedworkflows/WorkflowRunnerService.cs
@@ -12,7 +12,6 @@ using UiPath.Testing.Enums;
 using UiPath.UIAutomationNext.API.Contracts;
 using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
-using SecurityCode.ObjectRepository;
 using System.Threading.Tasks;
 using UiPath.Activities.Contracts;
 
@@ -41,24 +40,6 @@ namespace SecurityCode
         {
             var result = _runWorkflowHandler(@"GeraCodigoSeguranca.cs", new Dictionary<string, object>{{"in_inputHash", in_inputHash}}, default, default, default);
             return ((string)result["out_strCodigoSha1"], (int)result["out_intTamanhoCodigo"]);
-        }
-
-        /// <summary>
-        /// Invokes the Base/DadosPessoais.cs
-        /// </summary>
-        public (string out_strTextoSha1, string out_strNome) DadosPessoais()
-        {
-            var result = _runWorkflowHandler(@"Base\DadosPessoais.cs", new Dictionary<string, object>{}, default, default, default);
-            return ((string)result["out_strTextoSha1"], (string)result["out_strNome"]);
-        }
-
-        /// <summary>
-        /// Invokes the GeraDadosPessoais.cs
-        /// </summary>
-        public (string out_strTextoSha1, string out_strNome) GeraDadosPessoais()
-        {
-            var result = _runWorkflowHandler(@"GeraDadosPessoais.cs", new Dictionary<string, object>{}, default, default, default);
-            return ((string)result["out_strTextoSha1"], (string)result["out_strNome"]);
         }
 
         /// <summary>
